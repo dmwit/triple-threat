4a577d665dc68b53daeb5b8dac99a5b75f305f3a
Daniel Wagner <daniel@wagner-home.com>
2013-02-26 11:57:42 -0500

    this is one sophisticated cat

--- Spreadsheet.hs
+++ Spreadsheet.hs
@@ -1,8 +1,11 @@
-{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, NoMonomorphismRestriction #-}
 module Spreadsheet where
 
+import Control.Applicative
 import Data.Map
 import Data.Tree
+import System.Environment
+import Text.Parsec hiding (many, optional, (<|>))
 
 type CellName = String
 type Value    = Integer
@@ -14,6 +17,8 @@ data Formula
 	= Cell CellName
 	| BinOp Op Formula Formula
 	deriving (Eq, Ord, Show, Read)
+
+data Equation = Equation CellName Formula deriving (Eq, Ord, Show, Read)
 
 data Op = Plus | Minus deriving (Eq, Ord, Show, Read)
 
@@ -35,6 +40,26 @@ instance PPrint Op where
 	pprint Plus  = "+"
 	pprint Minus = "-"
 
+parseCellName = many1 (noneOf " >")
+
+class    Parseable a       where parser :: Stream s m Char => ParsecT s u m a
+instance Parseable Op      where parser = (string "+" >> return Plus) <|> (string "-" >> return Minus)
+instance Parseable Formula where
+	parser = chainl1 (parens <|> cell) (BinOp <$> parser) where
+		cell   = string "<" *> (Cell <$> parseCellName) <* string ">"
+		parens = string "(" *> parser <* string ")"
+
+instance Parseable Equation where
+	parser = do
+		n <- parseCellName
+		string " = "
+		f <- parser
+		return (Equation n f)
+
+instance Parseable SpreadsheetFormulas where
+	parser = munge <$> many (optional parser <* string "\n") where
+		munge xs = fromList [(n,f) | Just (Equation n f) <- xs]
+
 data Spreadsheet = Spreadsheet
 	{ formulas     :: SpreadsheetFormulas
 	, values       :: SpreadsheetValues
@@ -48,3 +73,13 @@ step :: Spreadsheet -> CellName -> Value -> Spreadsheet
 addFormula = undefined
 finalize = undefined
 step = undefined
+
+main = do
+	a <- getArgs
+	case a of
+		[fileName] -> do
+			s <- readFile fileName
+			case parse parser fileName s of
+				Left  err -> print err
+				Right eqs -> putStr . pprint $ (eqs :: SpreadsheetFormulas)
+		_ -> putStrLn "call with one argument naming a file with a bunch of equations, one on each line"
