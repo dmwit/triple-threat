1e0b1a8ea2c35d6cbf2b03b8ab8fe9f25f076727
Daniel Wagner <daniel@wagner-home.com>
2013-04-23 16:17:45 -0400

    clean up a bit + let it build

--- SpreadsheetWidget.hs
+++ SpreadsheetWidget.hs
@@ -1,62 +1,57 @@
 module SpreadsheetWidget where
 
 import SpreadsheetCommon
+import Data.Map (Map)
+import Data.Set (Set)
 
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 
-type CellDomain = Set.Set CellName
-type DangerZone = Set.Set Value -> Bool
+type CellDomain = Set CellName
+type DangerZone = Set (Set CellName)
 type Method     = SpreadsheetValues -> SpreadsheetValues
 
 data Widget = Widget
-  { dom :: CellDomain
-  , rel :: SpreadsheetValues -> Bool
-  , danger :: DangerZone
-  , methods :: Data.Map CellDomain [Method]
-  } deriving (Eq, Ord, Show, Read)
-             
+  { domain    :: CellDomain
+  , invariant :: SpreadsheetValues -> Bool
+  , danger    :: DangerZone
+  , methods   :: CellDomain -> Method
+  }
 
 inDom :: SpreadsheetValues -> CellDomain -> Bool
-inDom vals dom =
-  Set.isSubsetOf (Map.keysSet vals) dom
-  
+inDom vals dom = Set.isSubsetOf (Map.keysSet vals) dom
+
 restrictDom :: SpreadsheetValues -> CellDomain -> SpreadsheetValues
-restrictDom vals b =
-  let a    = Map.keysSet vals in
-  let diff = Set.difference b a in
-  Set.fold g vals diff where
-    g name vals = Map.update (\val -> Nothing) name vals
-              
-composeMethodsOrdered :: CellDomain -> Method -> Method -> Method
-composeMethodsOrdered x m1 m2 =
-  let apply1 vals = overwrite vals m1 in
-  let apply2 vals = overwrite vals m2 in
-  apply2 . apply1
-  
--- overwriteValues s1 s2 = s3 provided 
--- for all x \in dom(s1), s3(x) = s1(x)
--- for all x \in dom(s2) - dom(s1), s3(x) = s2(x)
--- for all other x, dom(s3) = Nothing
-overwriteValues :: SpreadsheetValues -> SpreadsheetValues -> SpreadsheetValues
-overwriteValues = undefined
-    
+restrictDom vals b = Map.intersection vals (Map.fromSet (const ()) b)
+
+-- Minimize the representation of a danger zone.
+-- Recall that representations can be turned into real danger zones via the transformation:
+--
+-- toZone :: Set (Set CellName) -> (Set CellName -> Bool)
+-- toZone rep x = any [x `Set.isSubsetOf` r | r <- Set.toList rep]
+--
+-- Given a representation rep, this function attempts to produce a smaller
+-- representation rep' such that toZone rep = toZone rep'. At the moment, it
+-- does this only by iterating one rule: remove any sets of names which are
+-- proper supersets of another set of names in the representation. To be
+-- complete, we would probably also need a rule that said something like "If
+-- all of the sets of names S U {x} for x in the domain of the lens are in the
+-- representation, replace them with the single set S.". We don't try to do
+-- that yet.
+--
+-- TODO: in fact, we don't even do the easy rule yet!
+minimizeDangerZone x = x
+
 compose :: Widget -> Widget -> Widget
-compose k l =
-  let dom_k = dom k in
-  let dom_l = dom l in
-  let dom'  = Set.union dom_k dom_l in
-  let rel'  = \ vals -> 
-        inDom vals dom && 
-        rel k (restrictDom vals dom_k) &&
-        rel l (restrictDom vals dom_l) in
-  let dz_k     = danger k in
-  let dz_l     = danger l in
-  let danger'  = (dz_k (Set.union) dz_l) (Set.difference) (dom_k (Set.intersection) dom_l) in
-  
-  let in_k x = Set.intersect x dom_l in
-  let in_l x = (x (Set.intersect) dom_l) (Set.union) (dom_k (Set.intersect) dom_l) in
-  let methods' = undefined in
-  Widget dom' rel' danger' methods'
-  
-        
+compose
+  ( Widget { domain = domk, invariant = invk, danger = dzk, methods = fk })
+  ( Widget { domain = doml, invariant = invl, danger = dzl, methods = fl })
+  = Widget { domain = dom , invariant = inv , danger = dz , methods = f  } where
+  dom      = Set.union domk doml
+  inv vals = invk (restrictDom vals domk) && invl (restrictDom vals doml)
+  dz       = minimizeDangerZone (dzk `Set.union` dzl `Set.union` Set.fromList
+                                   [ (vk `Set.union` vl) `Set.difference` (domk `Set.intersection` doml)
+                                   | vk <- Set.toList dzk
+                                   , vl <- Set.toList dzl
+                                   ])
+  f = undefined
